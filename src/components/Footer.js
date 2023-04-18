aa05b3124f8449900c031035ee6f1e2a6cbf7d9f
Achinta <achintadutta1997@gmail.com>
2023-04-19 02:31:13 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+let currentYear=new Date().getFullYear();
+
+function Footer(){
+    return (
+        // {currentYear}
+        <h3>Testing conditional rendering</h3>
+    );
+}
+
+export default Footer;
